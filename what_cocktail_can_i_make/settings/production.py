febd2ea1458edf055ce22dac1f3488a7adbdb78f
dannytarn <denys.tarnavskyi@wesoftyou.com>
2022-01-22 00:44:51 +0200

    s3 try

--- what_cocktail_can_i_make/settings/production.py
+++ what_cocktail_can_i_make/settings/production.py
@@ -17,3 +17,18 @@ def get_ec2_instance_ip():
 
 AWS_LOCAL_IP = get_ec2_instance_ip() or '*'
 ALLOWED_HOSTS = ['cocktails-dev.eu-central-1.elasticbeanstalk.com', AWS_LOCAL_IP]
+
+
+if 'AWS_ACCESS_KEY_ID' in os.environ:
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
