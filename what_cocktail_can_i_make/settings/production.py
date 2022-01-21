3d0996113ddc678bfb117b5da9798ae650fa1222
dannytarn <denys.tarnavskyi@wesoftyou.com>
2022-01-21 23:39:26 +0200

    get ec2 ip for allowed_hosts

--- what_cocktail_can_i_make/settings/production.py
+++ what_cocktail_can_i_make/settings/production.py
@@ -1,4 +1,19 @@
+import requests
+
 from .development import *
 
 
-ALLOWED_HOSTS = ['*']
+def get_ec2_instance_ip():
+    """Try to obtain the IP address of the current EC2 instance in AWS"""
+    try:
+        ip = requests.get(
+          'http://169.254.169.254/latest/meta-data/local-ipv4',
+          timeout=0.01
+        ).text
+    except requests.exceptions.ConnectionError:
+        return None
+    return ip
+
+
+AWS_LOCAL_IP = get_ec2_instance_ip() or '*'
+ALLOWED_HOSTS = ['cocktails-dev.eu-central-1.elasticbeanstalk.com', AWS_LOCAL_IP]
